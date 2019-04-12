d37c51c49fc211e877d4f0d6add13a08f436b61d
boxshadow <idafoo@sina.com>
2019-04-12 20:19:32 +0800

    add: hot构建

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,45 +1,31 @@
-let mix = require('laravel-mix');
-let path = require('path');
+let mix = require('laravel-mix')
+let path = require('path')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 mix.setPublicPath(path.join('public', 'assets'))
-  .setResourceRoot('assets/')
+  .setResourceRoot(mix.config.hmr ? '' : 'assets/')
   .webpackConfig({
     resolve: {
       extensions: ['.js', '.vue', '.json'],
       alias: {
-        "@": path.resolve(
+        '@': path.resolve(
           __dirname,
-          "resources/assets/admin"
+          'resources/assets/admin'
         )
       }
     },
     output: {
-      // 依据该路径进行编译以及异步加载
-      publicPath: 'assets/',
-      // 注意开发期间不加 hash，以免自动刷新失败
-      chunkFilename: `js/chunk-[name].${mix.inProduction() ? '-[chunkhash].' : ''}js`
+      chunkFilename: mix.config.hmr
+        ? `[name].js`
+        : (`js/chunk-[name].${mix.inProduction() ? '-[chunkhash].' : ''}js`),
+      publicPath: mix.config.hmr ? '//localhost:8080/' : 'assets/'
     },
-  });
-/*
- |--------------------------------------------------------------------------
- | 后台可运行 js 捆
- |--------------------------------------------------------------------------
- |
- | 不包含 jQuery 和 Bootstrap 的 vue 捆包。
- |
- */
+    devServer: {
+      // contentBase: path.resolve(__dirname, 'public/assets/'),
+      disableHostCheck: true
+    }
+  })
 
-mix.js('resources/assets/admin/main.js', path.join('public', 'assets', 'js', 'admin.js'));
+mix.js('resources/assets/admin/main.js', path.join('public', 'assets', 'js', 'admin.js'))
 
 if (mix.inProduction()) {
   mix.version()
