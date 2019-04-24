34075df4ac67f31762a1340e1531137d4cff7f99
boxshadow <idafoo@sina.com>
2019-04-24 11:54:02 +0800

    del: 去除站点配置页面的折叠块

--- resources/assets/admin/views/tsplus/operation/basic/setting.vue
+++ resources/assets/admin/views/tsplus/operation/basic/setting.vue
@@ -1,96 +1,83 @@
 <template>
   <el-main v-loading="getLoading">
+    <el-alert class="setting-alert" :closable="false" title="修改配置后,别忘了保存哦" type="warning" show-icon></el-alert>
     <el-form :model="setting" ref="setting" label-width="80px">
-      <el-collapse v-model="activeNames">
-        <el-collapse-item title="积分设置" name="1">
-          <el-alert
-            class="setting-alert"
-            :closable="false"
-            title="启动规则，用户完成相应的节点操作可以获取对应的奖励，关闭规则，用户完成相应的节点操作不能获取对应的奖励"
-            type="warning"
-            show-icon
-          ></el-alert>
-          <el-form-item prop="gold.status" label="积分开关">
-            <el-radio-group v-model="setting.gold.status">
-              <el-radio
-                v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
-                :key="item.key"
-                :label="item.label"
-              >{{item.title}}</el-radio>
-            </el-radio-group>
-          </el-form-item>
-        </el-collapse-item>
-        <el-collapse-item title="打赏设置" name="2">
-          <el-alert
-            class="setting-alert"
-            :closable="false"
-            title="用户打赏开启与关闭"
-            type="warning"
-            show-icon
-          ></el-alert>
-          <el-form-item prop="reward.status" label="打赏开关">
-            <el-radio-group v-model="setting.reward.status">
-              <el-radio
-                v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
-                :key="item.key"
-                :label="item.label"
-              >{{item.title}}</el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-alert
-            class="setting-alert"
-            :closable="false"
-            title="打赏金额配置，单位(分)。最少为1分，多个金额用英文半角符号“,”分割 例如：5,10,15。"
-            type="warning"
-            show-icon
-          ></el-alert>
-          <el-form-item prop="reward.amounts" label="打赏金额">
-            <el-input v-model="setting.reward.amounts"></el-input>
-          </el-form-item>
-        </el-collapse-item>
-        <el-collapse-item title="昵称设置" name="3">
-          <el-alert
-            class="setting-alert"
-            :closable="false"
-            title="站点是否开启匿名，开启后用户可以选择匿名操作部分内容"
-            type="warning"
-            show-icon
-          ></el-alert>
-          <el-form-item prop="anonymous.status" label="匿名开关">
-            <el-radio-group v-model="setting.anonymous.status">
-              <el-radio
-                v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
-                :key="item.key"
-                :label="item.label"
-              >{{item.title}}</el-radio>
-            </el-radio-group>
-          </el-form-item>
-          <el-alert
-            class="setting-alert"
-            :closable="false"
-            title="不能被用户注册的昵称, 多个昵称用半角「,」隔开"
-            type="warning"
-            show-icon
-          ></el-alert>
-          <el-form-item prop="reserved_nickname" label="预留昵称">
-            <el-input v-model="setting.reserved_nickname" placeholder></el-input>
-          </el-form-item>
-        </el-collapse-item>
-        <el-collapse-item title="客服设置" name="4">
-          <el-form-item prop="client_email" label="客服邮箱">
-            <el-input v-model="setting.client_email" placeholder="客服邮箱"></el-input>
-          </el-form-item>
-        </el-collapse-item>
-        <el-collapse-item title="其他设置" name="5">
-          <el-form-item prop="user_invite_template" label="邀请模板">
-            <el-input
-              type="textarea"
-              v-model="setting.user_invite_template"
-              placeholder="短信邀请模板, App端使用"
-            ></el-input>
-          </el-form-item>
-        </el-collapse-item>
-      </el-collapse>
+      <el-alert
+        class="setting-alert"
+        :closable="false"
+        title="启动规则，用户完成相应的节点操作可以获取对应的奖励，关闭规则，用户完成相应的节点操作不能获取对应的奖励"
+        type="warning"
+        show-icon
+      ></el-alert>
+      <el-form-item prop="gold.status" label="积分开关">
+        <el-radio-group v-model="setting.gold.status">
+          <el-radio
+            v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
+            :key="item.key"
+            :label="item.label"
+          >{{item.title}}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-alert class="setting-alert" :closable="false" title="用户打赏开启与关闭" type="warning" show-icon></el-alert>
+      <el-form-item prop="reward.status" label="打赏开关">
+        <el-radio-group v-model="setting.reward.status">
+          <el-radio
+            v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
+            :key="item.key"
+            :label="item.label"
+          >{{item.title}}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-alert
+        class="setting-alert"
+        :closable="false"
+        title="打赏金额配置，单位(分)。最少为1分，多个金额用英文半角符号“,”分割 例如：5,10,15。"
+        type="warning"
+        show-icon
+      ></el-alert>
+      <el-form-item prop="reward.amounts" label="打赏金额">
+        <el-input v-model="setting.reward.amounts"></el-input>
+      </el-form-item>
+
+      <el-alert
+        class="setting-alert"
+        :closable="false"
+        title="站点是否开启匿名，开启后用户可以选择匿名操作部分内容"
+        type="warning"
+        show-icon
+      ></el-alert>
+      <el-form-item prop="anonymous.status" label="匿名开关">
+        <el-radio-group v-model="setting.anonymous.status">
+          <el-radio
+            v-for="item in [{label: false, title: '关闭'},{label: true, title: '开启'}]"
+            :key="item.key"
+            :label="item.label"
+          >{{item.title}}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-alert
+        class="setting-alert"
+        :closable="false"
+        title="不能被用户注册的昵称, 多个昵称用半角「,」隔开"
+        type="warning"
+        show-icon
+      ></el-alert>
+      <el-form-item prop="reserved_nickname" label="预留昵称">
+        <el-input v-model="setting.reserved_nickname" placeholder></el-input>
+      </el-form-item>
+
+      <el-form-item prop="client_email" label="客服邮箱">
+        <el-input v-model="setting.client_email" placeholder="客服邮箱"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="user_invite_template" label="邀请模板">
+        <el-input
+          type="textarea"
+          v-model="setting.user_invite_template"
+          placeholder="短信邀请模板, App端使用"
+        ></el-input>
+      </el-form-item>
       <el-form-item class="submit-item">
         <el-button
           type="primary"
@@ -172,6 +159,7 @@ export default {
   margin-top: 20px;
 }
 .setting-alert {
+  max-width: 500px;
   margin-bottom: 10px;
 }
 </style>
