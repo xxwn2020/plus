85616656639a455de07fe629462800b15a6ddbdb
Seven Du <shiweidu@outlook.com>
2019-05-05 06:58:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/APIs/V2/UserController.php
+++ app/Http/Controllers/APIs/V2/UserController.php
@@ -26,203 +26,202 @@ use Tymon\JWTAuth\JWTAuth;
 use Zhiyi\Plus\Models\User;
 use Illuminate\Http\Request;
 use Zhiyi\Plus\Models\Taggable;
+use function Zhiyi\Plus\setting;
+use function Zhiyi\Plus\username;
 use Zhiyi\Plus\Models\VerificationCode;
 use Zhiyi\Plus\Http\Requests\API2\StoreUserPost;
 use Illuminate\Contracts\Routing\ResponseFactory as ResponseFactoryContract;
-use function Zhiyi\Plus\setting;
-use function Zhiyi\Plus\username;
 
 class UserController extends Controller
 {
-  /**
-   * Get all users.
-   *
-   * @param  Request  $request
-   * @param  ResponseFactoryContract  $response
-   * @param  User  $model
-   *
-   * @return mixed
-   * @throws Throwable
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function index(
+    /**
+     * Get all users.
+     *
+     * @param  Request  $request
+     * @param  ResponseFactoryContract  $response
+     * @param  User  $model
+     *
+     * @return mixed
+     * @throws Throwable
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function index(
     Request $request,
     ResponseFactoryContract $response,
     User $model
   ) {
-    $user = $request->user('api');
-    $ids = array_filter(explode(',', $request->query('id', '')));
-    $limit = max(min($request->query('limit', 15), 50), 1);
-    $order = in_array($order = $request->query('order', 'desc'),
+        $user = $request->user('api');
+        $ids = array_filter(explode(',', $request->query('id', '')));
+        $limit = max(min($request->query('limit', 15), 50), 1);
+        $order = in_array($order = $request->query('order', 'desc'),
       ['asc', 'desc']) ? $order : 'desc';
-    $since = $request->query('since', false);
-    $name = $request->query('name', false);
-    $fetchBy = $request->query('fetch_by', 'id');
-    $tags = $request->query('tags', []);
+        $since = $request->query('since', false);
+        $name = $request->query('name', false);
+        $fetchBy = $request->query('fetch_by', 'id');
+        $tags = $request->query('tags', []);
 
-    $users = $model
+        $users = $model
       ->when($since, function ($query) use ($since, $order) {
-        return $query->where('id', $order === 'asc' ? '>' : '<', $since);
+          return $query->where('id', $order === 'asc' ? '>' : '<', $since);
       })
       ->when($name && $fetchBy !== 'username', function ($query) use ($name) {
-        return $query->where('name', 'like', sprintf('%%%s%%', $name));
+          return $query->where('name', 'like', sprintf('%%%s%%', $name));
       })
       ->when(! empty($ids) && $fetchBy === 'id', function ($query) use ($ids) {
-        return $query->whereIn('id', $ids)->withTrashed();
+          return $query->whereIn('id', $ids)->withTrashed();
       })
       ->when($name && $fetchBy === 'username', function ($query) use ($name) {
-        return $query->whereIn('name', array_filter(explode(',', $name)));
+          return $query->whereIn('name', array_filter(explode(',', $name)));
       })
       ->when(is_array($tags) && ! empty($tags), function ($query) use ($tags) {
-        return $query->whereHas('tags', function ($query) use ($tags) {
-          $taggableTable = (new Taggable)->getTable();
+          return $query->whereHas('tags', function ($query) use ($tags) {
+              $taggableTable = (new Taggable)->getTable();
 
-          return $query->whereIn($taggableTable.'.tag_id', $tags);
-        });
+              return $query->whereIn($taggableTable.'.tag_id', $tags);
+          });
       })
       ->limit($limit)
       ->orderby('id', $order)
       ->get();
-    $users->load(['certification']);
+        $users->load(['certification']);
 
-    return $response->json($model->getConnection()->transaction(function () use
-    (
+        return $response->json($model->getConnection()->transaction(function () use (
       $users,
       $user
     ) {
-      return $users->map(function (User $item) use ($user) {
-        $item->following = $item->hasFollwing($user->id ?? 0);
-        $item->follower = $item->hasFollower($user->id ?? 0);
-        $item->blacked = $user ? $user->blacked($item) : false;
+            return $users->map(function (User $item) use ($user) {
+                $item->following = $item->hasFollwing($user->id ?? 0);
+                $item->follower = $item->hasFollower($user->id ?? 0);
+                $item->blacked = $user ? $user->blacked($item) : false;
 
-        return $item;
-      });
-    }))->setStatusCode(200);
-  }
+                return $item;
+            });
+        }))->setStatusCode(200);
+    }
 
-  /**
-   *  Get user.
-   *
-   * @param  Request  $request
-   * @param  string  $user
-   *
-   * @return mixed
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function show(Request $request, string $user)
-  {
-    $field = username($user);
-    $user = User::withTrashed()
+    /**
+     *  Get user.
+     *
+     * @param  Request  $request
+     * @param  string  $user
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function show(Request $request, string $user)
+    {
+        $field = username($user);
+        $user = User::withTrashed()
       ->where($field, $user)
       ->firstOrFail();
 
-    $user->makeVisible($field);
+        $user->makeVisible($field);
 
-    // 我关注的处理
-    $this->hasFollowing($request, $user);
-    // 处理关注我的
-    $this->hasFollower($request, $user);
-    // 处理黑名单
-    $this->hasBlacked($request, $user);
+        // 我关注的处理
+        $this->hasFollowing($request, $user);
+        // 处理关注我的
+        $this->hasFollower($request, $user);
+        // 处理黑名单
+        $this->hasBlacked($request, $user);
 
-    return response()->json($user, 200);
-  }
+        return response()->json($user, 200);
+    }
 
-  /**
-   * 创建用户.
-   *
-   * @return mixed
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function store(
+    /**
+     * 创建用户.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function store(
     StoreUserPost $request,
     ResponseFactoryContract $response,
     JWTAuth $auth
   ) {
-    $phone = $request->input('phone');
-    $email = $request->input('email');
-    $name = $request->input('name');
-    $password = $request->input('password');
-    $channel = $request->input('verifiable_type');
-    $code = $request->input('verifiable_code');
-    $role = setting('user', 'register-role');
+        $phone = $request->input('phone');
+        $email = $request->input('email');
+        $name = $request->input('name');
+        $password = $request->input('password');
+        $channel = $request->input('verifiable_type');
+        $code = $request->input('verifiable_code');
+        $role = setting('user', 'register-role');
 
-    if ( ! $role) {
-      throw new RuntimeException('Failed to get the defined user group.');
-    }
+        if (! $role) {
+            throw new RuntimeException('Failed to get the defined user group.');
+        }
 
-    $verify = VerificationCode::where('account',
+        $verify = VerificationCode::where('account',
       $channel == 'mail' ? $email : $phone)
       ->where('channel', $channel)
       ->where('code', $code)
       ->orderby('id', 'desc')
       ->first();
 
-    if ( ! $verify) {
-      return $response->json(['message' => ['验证码错误或者已失效']], 422);
-    }
+        if (! $verify) {
+            return $response->json(['message' => ['验证码错误或者已失效']], 422);
+        }
 
-    $user = new User();
-    $user->phone = $phone;
-    $user->email = $email;
-    $user->name = $name;
-    $user->register_ip = $request->ip();
+        $user = new User();
+        $user->phone = $phone;
+        $user->email = $email;
+        $user->name = $name;
+        $user->register_ip = $request->ip();
 
-    if ($password !== null) {
-      $user->createPassword($password);
-    }
+        if ($password !== null) {
+            $user->createPassword($password);
+        }
 
-    $verify->delete();
-    if ( ! $user->save()) {
-      return $response->json(['message' => '注册失败'], 500);
-    }
+        $verify->delete();
+        if (! $user->save()) {
+            return $response->json(['message' => '注册失败'], 500);
+        }
 
-    $user->roles()->sync($role);
+        $user->roles()->sync($role);
 
-    return $response->json([
+        return $response->json([
       'token'       => $auth->fromUser($user),
       'ttl'         => config('jwt.ttl'),
       'refresh_ttl' => config('jwt.refresh_ttl'),
     ])->setStatusCode(201);
-  }
+    }
 
-  /**
-   * Handle the state of my follow status.
-   *
-   * @param  Request  $request
-   * @param  User &$user
-   *
-   * @return void
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  protected function hasFollowing(Request $request, User &$user)
-  {
-    $currentUser = $request->user('api');
-    $hasUser = (int) $request->query('following',
+    /**
+     * Handle the state of my follow status.
+     *
+     * @param  Request  $request
+     * @param  User &$user
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function hasFollowing(Request $request, User &$user)
+    {
+        $currentUser = $request->user('api');
+        $hasUser = (int) $request->query('following',
       $currentUser ? $currentUser->id : 0);
-    $user['following'] = $user->hasFollwing($hasUser);
-  }
+        $user['following'] = $user->hasFollwing($hasUser);
+    }
 
-  /**
-   * Verify that I am followed.
-   *
-   * @param  Request  $request
-   * @param  User &$user
-   *
-   * @return void
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  protected function hasFollower(Request $request, User &$user)
-  {
-    $currentUser = $request->user('api');
-    $hasUser = (int) $request->query('follower',
+    /**
+     * Verify that I am followed.
+     *
+     * @param  Request  $request
+     * @param  User &$user
+     *
+     * @return void
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function hasFollower(Request $request, User &$user)
+    {
+        $currentUser = $request->user('api');
+        $hasUser = (int) $request->query('follower',
       $currentUser ? $currentUser->id : 0);
-    $user['follower'] = $user->hasFollower($hasUser);
-  }
+        $user['follower'] = $user->hasFollower($hasUser);
+    }
 
-  protected function hasBlacked(Request $request, User &$user)
-  {
-    $currentUser = $request->user('api');
-    $user['blacked'] = $currentUser ? $currentUser->blacked($user) : false;
-  }
+    protected function hasBlacked(Request $request, User &$user)
+    {
+        $currentUser = $request->user('api');
+        $user['blacked'] = $currentUser ? $currentUser->blacked($user) : false;
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -32,14 +32,14 @@ use Zhiyi\Plus\FileStorage\Traits\EloquentAttributeTrait as FileStorageEloquentA
 
 class User extends Authenticatable implements JWTSubject
 {
-  // 功能性辅助相关。
-  use Notifiable,
+    // 功能性辅助相关。
+    use Notifiable,
     SoftDeletes,
     Concerns\UserHasAbility,
     Concerns\UserHasNotifiable,
     Concerns\Macroable;
-  // 关系数据相关
-  use Relations\UserHasWallet,
+    // 关系数据相关
+    use Relations\UserHasWallet,
     Relations\UserHasWalletCash,
     Relations\UserHasWalletCharge,
     Relations\UserHasFilesWith,
@@ -51,286 +51,293 @@ class User extends Authenticatable implements JWTSubject
     Relations\UserHasCurrency,
     Relations\UserHasNewWallet,
     Relations\UserHasBlackList;
-  use FileStorageEloquentAttributeTrait;
-  /**
-   * The attributes that are mass assignable.
-   *
-   * @var array
-   */
-  protected $fillable
+    use FileStorageEloquentAttributeTrait;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable
     = [
       'name', 'email', 'phone', 'password', 'last_login_ip', 'register_ip',
     ];
-  /**
-   * The attributes that should be hidden for arrays.
-   *
-   * @var array
-   */
-  protected $hidden
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden
     = [
       'password', 'remember_token', 'phone', 'email', 'pivot',
     ];
-  /**
-   * The accessors to append to the model's array form.
-   *
-   * @var array
-   */
-  protected $appends = ['verified'];
-  /**
-   * The relations to eager load on every query.
-   *
-   * @var array
-   */
-  protected $with = ['extra'];
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['verified'];
+    /**
+     * The relations to eager load on every query.
+     *
+     * @var array
+     */
+    protected $with = ['extra'];
 
-  /**
-   * Get Notification for JPush sender.
-   *
-   * @return Sender
-   */
-  protected function routeNotificationForJpush()
-  {
-    return new Sender([
+    /**
+     * Get Notification for JPush sender.
+     *
+     * @return Sender
+     */
+    protected function routeNotificationForJpush()
+    {
+        return new Sender([
       'platform' => 'all',
       'audience' => [
         'alias' => sprintf('user_%d', $this->id),
       ],
     ]);
-  }
-
-  /**
-   * Get the identifier that will be stored in the subject claim of the JWT.
-   *
-   * @return mixed
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function getJWTIdentifier()
-  {
-    return $this->getKey();
-  }
-
-  /**
-   * Return a key value array, containing any custom claims to be added to the JWT.
-   *
-   * @return array
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function getJWTCustomClaims()
-  {
-    return [];
-  }
-
-  protected function getAvatarAttribute(?string $resource)
-  : ?FileMetaInterface {
-    if ( ! $resource) {
-      return null;
     }
 
-    return $this->getFileStorageResourceMeta($resource);
-  }
-
-  protected function getBgAttribute(?string $resource)
-  : ?FileMetaInterface {
-    if ( ! $resource) {
-      return null;
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
 
-    return $this->getFileStorageResourceMeta($resource);
-  }
-
-  /**
-   * Get verified.
-   *
-   * @return array|null
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  protected function getVerifiedAttribute()
-  {
-    $certification = $this->certification;
-
-    if ( ! $certification || $certification->status !== 1) {
-      return null;
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 
-    return [
+    protected function getAvatarAttribute(?string $resource)
+  : ?FileMetaInterface
+    {
+        if (! $resource) {
+            return null;
+        }
+
+        return $this->getFileStorageResourceMeta($resource);
+    }
+
+    protected function getBgAttribute(?string $resource)
+  : ?FileMetaInterface
+    {
+        if (! $resource) {
+            return null;
+        }
+
+        return $this->getFileStorageResourceMeta($resource);
+    }
+
+    /**
+     * Get verified.
+     *
+     * @return array|null
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    protected function getVerifiedAttribute()
+    {
+        $certification = $this->certification;
+
+        if (! $certification || $certification->status !== 1) {
+            return null;
+        }
+
+        return [
       'type'        => $certification->certification_name,
       'icon'        => $certification->icon,
       'description' => $certification->data['desc'] ?? '',
     ];
-  }
+    }
 
-  /**
-   * Has user extra.
-   *
-   * @return HasOne
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function extra()
-  {
-    return $this->hasOne(UserExtra::class, 'user_id', 'id');
-  }
+    /**
+     * Has user extra.
+     *
+     * @return HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function extra()
+    {
+        return $this->hasOne(UserExtra::class, 'user_id', 'id');
+    }
 
-  /**
-   * Has user certification.
-   *
-   * @return HasOne
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function certification()
-  {
-    return $this->hasOne(Certification::class, 'user_id', 'id');
-  }
+    /**
+     * Has user certification.
+     *
+     * @return HasOne
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function certification()
+    {
+        return $this->hasOne(Certification::class, 'user_id', 'id');
+    }
 
-  /**
-   * Has tags of the user.
-   *
-   * @return MorphToMany
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function tags()
-  {
-    return $this->morphToMany(Tag::class, 'taggable', 'taggables')
+    /**
+     * Has tags of the user.
+     *
+     * @return MorphToMany
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable', 'taggables')
       ->withTimestamps();
-  }
+    }
 
-  /**
-   * 是否被后台推荐.
-   *
-   * @return HasOne [type] [description]
-   */
-  public function recommended()
-  {
-    return $this->hasOne(UserRecommended::class, 'user_id');
-  }
+    /**
+     * 是否被后台推荐.
+     *
+     * @return HasOne [type] [description]
+     */
+    public function recommended()
+    {
+        return $this->hasOne(UserRecommended::class, 'user_id');
+    }
 
-  /**
-   * 后台设置被注册者关注，或者是双向关注.
-   *
-   * @return HasOne [type] [description]
-   */
-  public function famous()
-  {
-    return $this->hasOne(Famous::class, 'user_id');
-  }
+    /**
+     * 后台设置被注册者关注，或者是双向关注.
+     *
+     * @return HasOne [type] [description]
+     */
+    public function famous()
+    {
+        return $this->hasOne(Famous::class, 'user_id');
+    }
 
-  /**
-   * 复用设置手机号查询条件方法.
-   *
-   * @param  Builder  $query  查询对象
-   * @param  string  $phone  手机号码
-   *
-   * @return Builder 查询对象
-   *
-   * @author Seven Du <shiweidu@outlook.com>
-   * @homepage http://medz.cn
-   */
-  public function scopeByPhone(Builder $query, string $phone)
-  : Builder {
-    return $query->where('phone', $phone);
-  }
+    /**
+     * 复用设置手机号查询条件方法.
+     *
+     * @param  Builder  $query  查询对象
+     * @param  string  $phone  手机号码
+     *
+     * @return Builder 查询对象
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByPhone(Builder $query, string $phone)
+  : Builder
+    {
+        return $query->where('phone', $phone);
+    }
 
-  /**
-   * 复用设置用户名查询条件方法.
-   *
-   * @param  Builder  $query  查询对象
-   * @param  string  $name  用户名
-   *
-   * @return Builder 查询对象
-   *
-   * @author Seven Du <shiweidu@outlook.com>
-   * @homepage http://medz.cn
-   */
-  public function scopeByName(Builder $query, string $name)
-  : Builder {
-    return $query->where('name', $name);
-  }
+    /**
+     * 复用设置用户名查询条件方法.
+     *
+     * @param  Builder  $query  查询对象
+     * @param  string  $name  用户名
+     *
+     * @return Builder 查询对象
+     *
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function scopeByName(Builder $query, string $name)
+  : Builder
+    {
+        return $query->where('name', $name);
+    }
 
-  /**
-   * 复用 E-Mail 查询条件方法.
-   *
-   * @param  Builder  $query
-   * @param  string  $email  [description]
-   *
-   * @return Builder
-   * @author Seven Du <shiweidu@outlook.com>
-   */
-  public function scopeByEmail(Builder $query, string $email)
-  : Builder {
-    return $query->where('email', $email);
-  }
+    /**
+     * 复用 E-Mail 查询条件方法.
+     *
+     * @param  Builder  $query
+     * @param  string  $email  [description]
+     *
+     * @return Builder
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function scopeByEmail(Builder $query, string $email)
+  : Builder
+    {
+        return $query->where('email', $email);
+    }
 
-  /**
-   * Create user ppassword.
-   *
-   * @param  string  $password  user password
-   *
-   * @return self
-   *
-   * @throws BindingResolutionException
-   * @author Seven Du <shiweidu@outlook.com>
-   * @homepage http://medz.cn
-   */
-  public function createPassword(string $password)
-  : self {
-    $this->password = app('hash')->make($password);
+    /**
+     * Create user ppassword.
+     *
+     * @param  string  $password  user password
+     *
+     * @return self
+     *
+     * @throws BindingResolutionException
+     * @author Seven Du <shiweidu@outlook.com>
+     * @homepage http://medz.cn
+     */
+    public function createPassword(string $password)
+  : self
+    {
+        $this->password = app('hash')->make($password);
 
-    return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * 验证用户密码
-   *
-   * @Author   Wayne[qiaobin@zhiyicx.com]
-   * @DateTime 2016-12-30T18:44:40+0800
-   *
-   * @param  string  $password  [description]
-   *
-   * @return bool 验证结果true or false
-   */
-  public function verifyPassword(string $password)
-  : bool {
-    return $this->password && app('hash')->check($password, $this->password);
-  }
+    /**
+     * 验证用户密码
+     *
+     * @Author   Wayne[qiaobin@zhiyicx.com]
+     * @DateTime 2016-12-30T18:44:40+0800
+     *
+     * @param  string  $password  [description]
+     *
+     * @return bool 验证结果true or false
+     */
+    public function verifyPassword(string $password)
+  : bool
+    {
+        return $this->password && app('hash')->check($password, $this->password);
+    }
 
-  /**
-   * 用户未读数统计.
-   *
-   * @return mixed
-   * @author BS <414606094@qq.com>
-   */
-  public function unreadCount()
-  {
-    return $this->hasOne(UserUnreadCount::class, 'user_id', 'id');
-  }
+    /**
+     * 用户未读数统计.
+     *
+     * @return mixed
+     * @author BS <414606094@qq.com>
+     */
+    public function unreadCount()
+    {
+        return $this->hasOne(UserUnreadCount::class, 'user_id', 'id');
+    }
 
-  /**
-   * 被举报记录.
-   *
-   * @return MorphMany
-   * @author BS <414606094@qq.com>
-   */
-  public function reports()
-  {
-    return $this->morphMany(Report::class, 'reportable');
-  }
+    /**
+     * 被举报记录.
+     *
+     * @return MorphMany
+     * @author BS <414606094@qq.com>
+     */
+    public function reports()
+    {
+        return $this->morphMany(Report::class, 'reportable');
+    }
 
-  public function getImPwdHash()
-  {
-    return $this->password ? md5($this->password) : md5('123456');
-  }
+    public function getImPwdHash()
+    {
+        return $this->password ? md5($this->password) : md5('123456');
+    }
 
-  /**
-   * The user topics belong to many.
-   *
-   * @return BelongsToMany
-   */
-  public function feedTopics()
+    /**
+     * The user topics belong to many.
+     *
+     * @return BelongsToMany
+     */
+    public function feedTopics()
   : BelongsToMany
-  {
-    $table = (new FeedTopicUserLink)->getTable();
+    {
+        $table = (new FeedTopicUserLink)->getTable();
 
-    return $this
+        return $this
       ->belongsToMany(FeedTopic::class, $table, 'user_id', 'topic_id')
       ->using(FeedTopicUserLink::class);
-  }
+    }
 }
