5f34dba7a4b6dc98f93dc0e0cc731b8d74d92d1c
boxshadow <idafoo@sina.com>
2019-05-18 22:20:51 +0800

    暂存
    Signed-off-by: boxshadow <idafoo@sina.com>

--- resources/assets/admin/router/tsplus/index.js
+++ resources/assets/admin/router/tsplus/index.js
@@ -1,11 +1,11 @@
-import ManagementRouter from './modules/management'
-import OperationRouter from './modules/operation'
+// import ManagementRouter from './modules/management'
+// import OperationRouter from './modules/operation'
 import SystemRouter from './modules/system'
-import FinanceRouter from './modules/finance'
+// import FinanceRouter from './modules/finance'
 
 export default [
-  ManagementRouter,
-  OperationRouter,
-  SystemRouter,
-  FinanceRouter
+  // ManagementRouter,
+  // OperationRouter,
+  SystemRouter
+  // FinanceRouter
 ]
