1449d99fd1a9173d3ffda26f5b0d0def72025712
boxshadow <idafoo@sina.com>
2019-06-04 10:55:02 +0800

    chore: 开放路由

--- resources/assets/admin/router/tsplus/index.js
+++ resources/assets/admin/router/tsplus/index.js
@@ -1,15 +1,19 @@
-// import ManagementRouter from './modules/management'
-// import OperationRouter from './modules/operation'
-// import SystemRouter from './modules/system'
-// import FinanceRouter from './modules/finance'
-// import TemplateRouter from './modules/template'
+import ManagementRouter from './modules/management'
+import OperationRouter from './modules/operation'
+import SystemRouter from './modules/system'
+import FinanceRouter from './modules/finance'
+import TemplateRouter from './modules/template'
 import Report from './modules/report'
+import Promotion from './modules/promotion'
+import Currency from './modules/currency'
 
 export default [
-  // ManagementRouter,
-  // OperationRouter,
-  // SystemRouter
-  // FinanceRouter
-  // TemplateRouter
-  Report
+  ManagementRouter,
+  OperationRouter,
+  SystemRouter,
+  FinanceRouter,
+  TemplateRouter,
+  Report,
+  Promotion,
+  Currency
 ]
